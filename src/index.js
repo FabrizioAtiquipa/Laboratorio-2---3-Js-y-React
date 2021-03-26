494671904a1480ea09156f9856f4f55858347eb8
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-03-26 00:08:39 -0500

    Parte7 - Laboratorio 2 - 3: Js y React

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import Content from './Content ';
 import Total from './Total';
 
 const App = () => {
-
-    const course = 'Half Stack application development'
-    const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10
@@ -21,15 +21,15 @@ const App = () => {
         exercises: 14
       }
     ]
-  
-    return (
-      <div>
-        <Header course={course} />
-        <Content parts={parts} />
-        <Total parts={parts} />
-      </div>
-    )
   }
 
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
