b58094344c72e1678a69d5b6928870199f35527a
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-03-26 00:01:36 -0500

    Parte6 - Laboratorio 2 - 3: Js y React

--- src/Content .js
+++ src/Content .js
@@ -2,14 +2,17 @@ import React from 'react';
 import Part from './Part';
 
 export default function Content(props) {
-    //destructuring
-    const{part1, part2, part3}=props
-    const{name, exercises}=part1
+
     return(
         <div>
-            <Part enunciado={name} num={exercises} />
-            <Part enunciado={part2.name} num={part2.exercises} />
-            <Part enunciado={part3.name} num={part3.exercises} />
+         {
+             props.parts.map((ejem, index) => {
+                 return(
+                     <Part enunciado={ejem.name} num={ejem.exercises}></Part>
+                 );
+             })
+         }
         </div>
-    );
+     )
 }
+
